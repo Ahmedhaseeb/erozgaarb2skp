b783708447cd0eef7dfd9814f97ab808e4d140d2
Ahmed Haseeb <ahmedhaseeb123@gmail.com>
2019-07-23 10:02:21 +0500

    PHP Lecture 1

---
+++ php/index.php
@@ -0,0 +1,57 @@
+<?php 
+
+// adding two number
+
+// int x = 20;
+
+
+$var1 = "Ahmed";
+
+
+echo $var1;
+die();
+$team = [
+		"name",
+		645,
+		56756,
+		23423,
+		5.70765767
+	];
+
+
+$team[50.6] = "ABC";
+
+// print_r($team);
+
+echo count($team);
+
+
+// echo $team[4];
+
+
+
+die();
+$var1 = 2;
+$var2 = 3;
+
+$var1 = $var2;
+
+
+
+echo $var1;
+echo "<br>";
+echo $var2;
+
+// $x = 20;
+
+// $y = 30;
+
+
+// echo $x + $y;
+
+
+// echo 4 + 5;
+
+// echo "You have 2 new messages";
+
+?>
---
+++ php/lecture-1-class.php
@@ -0,0 +1,7 @@
+<?php
+$text="hellow world";
+$number="10";
+
+
+echo $text;//hellow world!
+echo $number;//10
